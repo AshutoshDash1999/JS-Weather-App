f5b8797061ea377f229128f1b44c3046b04f0220
AshutoshDash1999 <dashashutosh1999@gmail.com>
2023-09-11 00:24:31 +0530

    feat: weather icons added

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,8 +5,28 @@ export const showWeatherIcon = (weatherMain, weatherDescription) => {
   switch (weatherMain) {
     case "Clear":
       return;
-    case "Haze":
+    case "Haze" ||
+      "Mist" ||
+      "Tornado" ||
+      "Squall" ||
+      "Ash" ||
+      "Dust" ||
+      "Sand" ||
+      "Fog" ||
+      "Dust" ||
+      "Smoke" ||
+      "Mist":
       return `/assets/weather-icons/atmosphere/${dayType}.png`;
+    case "Snow":
+      return `/assets/weather-icons/snow/snow.png`;
+    case "Drizzle":
+      return `/assets/weather-icons/drizzle/${dayType}.png`;
+    case "Rain":
+      return `/assets/weather-icons/rain/${dayType}.png`;
+    case "Thunderstorm":
+      return `/assets/weather-icons/thunderstorm/thunderstorm.png`;
+    case "Clouds":
+      return `/assets/weather-icons/clouds/clouds.png`;
 
     default:
       break;
