2b7ba59672b24d22527d8f6563a7078db29b05a6
AshutoshDash1999 <dashashutosh1999@gmail.com>
2023-09-15 23:44:01 +0530

    feat: add daily forecast

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -8,38 +8,36 @@ const Forecast = () => {
 
   return (
     <section className="my-4 grid grid-cols-1 sm:grid-cols-4 md:grid-cols-7 gap-4">
-      {forecastData.length > 0 &&
-        forecastData?.map((daily, index) => {
-          const date = dayjs.unix(daily?.dt).local().format("DD MMM");
-          if (index + 1 < forecastData.length) {
-            return (
-              <div
-                className="bg-white rounded-xl flex flex-col items-center"
-                key={daily.dt}
-              >
-                <div className="flex justify-center p-2 h-24">
-                  <img
-                    src={showWeatherIcon(
-                      daily?.weather[0].main,
-                      daily?.weather[0].description
-                    )}
-                    alt="weather icon"
-                    height={100}
-                    width={100}
-                    className="object-contain"
-                  />
-                </div>
-                <div className="font-semibold text-neutral-500">{date}</div>
-                <div className="flex font-black text-neutral-700 text-2xl">
-                  {daily?.feels_like?.day}{" "}
-                  <IconTemperatureCelsius stroke={1.5} />{" "}
-                </div>
+      {forecastData?.map((daily, index) => {
+        const date = dayjs.unix(daily?.dt).local().format("DD MMM");
+        if (index + 1 < forecastData.length) {
+          return (
+            <div
+              className="bg-white rounded-xl flex flex-col items-center"
+              key={daily.dt}
+            >
+              <div className="flex justify-center p-2 h-24">
+                <img
+                  src={showWeatherIcon(
+                    daily?.weather[0].main,
+                    daily?.weather[0].description
+                  )}
+                  alt="weather icon"
+                  height={100}
+                  width={100}
+                  className="object-contain"
+                />
               </div>
-            );
-          } else {
-            return null;
-          }
-        })}
+              <div className="font-semibold text-neutral-500">{date}</div>
+              <div className="flex font-black text-neutral-700 text-2xl">
+                {daily?.feels_like?.day} <IconTemperatureCelsius stroke={1.5} />{" "}
+              </div>
+            </div>
+          );
+        } else {
+          return null;
+        }
+      })}
     </section>
   );
 };
