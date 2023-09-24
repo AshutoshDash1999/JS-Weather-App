294e14058432b70f4c8b779561075d0db2d32fbf
AshutoshDash1999 <dashashutosh1999@gmail.com>
2023-09-24 06:25:40 +0530

    add zustand store to local storage

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,37 +1,46 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-const useStore = create((set) => ({
-  weatherData: {},
-  setWeatherData: (data) => set({ weatherData: data }),
-  airPollutionData: {},
-  setAirPollutionData: (data) => set({ airPollutionData: data }),
-  forecastData: {},
-  setForecastData: (data) => set({ forecastData: data }),
-  alertData: [],
-  setAlertData: (data) => set({ alertData: data }),
-  userLocation: {
-    latitude: 0,
-    longitude: 0,
-  },
-  setUserLocation: (latitude, longitude) =>
-    set({
+const useStore = create(
+  persist(
+    (set) => ({
+      weatherData: {},
+      setWeatherData: (data) => set({ weatherData: data }),
+      airPollutionData: {},
+      setAirPollutionData: (data) => set({ airPollutionData: data }),
+      forecastData: {},
+      setForecastData: (data) => set({ forecastData: data }),
+      alertData: [],
+      setAlertData: (data) => set({ alertData: data }),
       userLocation: {
-        latitude,
-        longitude,
+        latitude: 0,
+        longitude: 0,
       },
+      setUserLocation: (latitude, longitude) =>
+        set({
+          userLocation: {
+            latitude,
+            longitude,
+          },
+        }),
+      userCity: "Bhubaneswar",
+      setUserCity: (city) => set({ userCity: city }),
+      isError: false,
+      setError: (error) =>
+        set({
+          isError: error,
+        }),
+      isDarkMode: false,
+      setDarkMode: (value) =>
+        set({
+          isDarkMode: value,
+        }),
     }),
-  userCity: "Bhubaneswar",
-  setUserCity: (city) => set({ userCity: city }),
-  isError: false,
-  setError: (error) =>
-    set({
-      isError: error,
-    }),
-  isDarkMode: false,
-  setDarkMode: (value) =>
-    set({
-        isDarkMode: value,
-    }),
-}));
+    {
+      name: "app-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+    }
+  )
+);
 
 export default useStore;
